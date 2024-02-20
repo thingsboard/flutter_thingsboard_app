f1f61fd2480cc21f6a394a67d5493cb410fe14bb
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2024-02-20 16:11:42 +0200

    Revert app_constants

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static final thingsBoardApiEndpoint = 'http://10.7.0.203:8080';
+  static final thingsBoardApiEndpoint = 'http://localhost:8080';
   static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   /// Not for production (only for debugging)
