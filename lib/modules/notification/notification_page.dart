f188bad6af5b241420a5bcb481f9a05e6508e884
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-02-28 17:02:56 +0200

    Added refresh indicator.

--- lib/modules/notification/notification_page.dart
+++ lib/modules/notification/notification_page.dart
@@ -57,166 +57,178 @@ class _NotificationPageState extends TbPageState<NotificationPage> {
             ),
           ],
         ),
-        body: ValueListenableBuilder<bool>(
-          valueListenable: _isLoadingNotifier,
-          builder: (context, loading, _) {
-            if (loading) {
-              return SizedBox.expand(
-                child: Container(
-                  color: Color(0x99FFFFFF),
-                  child: Center(
-                    child: TbProgressIndicator(
-                      tbContext,
-                      size: 50.0,
+        body: RefreshIndicator(
+          onRefresh: () {
+            return Future.value(_loadNotifications()).whenComplete(
+              () {
+                if (mounted) {
+                  setState(() {});
+                }
+              },
+            );
+          },
+          child: ValueListenableBuilder<bool>(
+            valueListenable: _isLoadingNotifier,
+            builder: (context, loading, _) {
+              if (loading) {
+                return SizedBox.expand(
+                  child: Container(
+                    color: Color(0x99FFFFFF),
+                    child: Center(
+                      child: TbProgressIndicator(
+                        tbContext,
+                        size: 50.0,
+                      ),
                     ),
                   ),
-                ),
-              );
-            } else {
-              if (_notifications.isEmpty) {
-                return Center(
+                );
+              } else {
+                if (_notifications.isEmpty) {
+                  return Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'No notifications yet',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 5,
+                    vertical: 10,
+                  ),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'No notifications yet',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: SegmentedButton(
+                          segments: [
+                            ButtonSegment(
+                              value: NotificationsFilter.unread,
+                              label: Center(
+                                child: Text(
+                                  'Unread',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            ButtonSegment(
+                              value: NotificationsFilter.all,
+                              label: Text(
+                                'All',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ],
+                          selected: {notificationsFilter},
+                          onSelectionChanged: (newSelection) {
+                            setState(() {
+                              notificationsFilter = newSelection.first;
+                            });
+                          },
+                          showSelectedIcon: false,
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.resolveWith(
+                              (states) {
+                                if (states.contains(MaterialState.selected)) {
+                                  return TbThemeUtils.tbPrimary;
+                                }
+
+                                return Colors.grey.withOpacity(0.3);
+                              },
+                            ),
+                            foregroundColor: MaterialStateProperty.resolveWith(
+                              (states) {
+                                if (states.contains(MaterialState.selected)) {
+                                  return Colors.white;
+                                }
+
+                                return Colors.grey;
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Expanded(
+                        child: NotificationsList(
+                          notifications: _notifications.reversed.where((e) {
+                            if (notificationsFilter ==
+                                NotificationsFilter.unread) {
+                              return !e.read;
+                            }
+
+                            return true;
+                          }).toList(),
+                          thingsboardClient: tbClient,
+                          tbContext: tbContext,
+                          onClearNotification: (id) {
+                            final notification = _notifications.firstWhere(
+                              (e) => e.message.messageId == id,
+                            );
+
+                            if (!notification.read) {
+                              NotificationService
+                                  .decreaseNotificationBadgeCount();
+                            }
+
+                            setState(() {
+                              _notifications.removeWhere(
+                                (e) => e.message.messageId == id,
+                              );
+                            });
+
+                            final storage = tbContext.storage;
+                            storage.setItem(
+                              NotificationService.notificationsListKey,
+                              jsonEncode(
+                                _notifications.map((e) => e.toJson()).toList(),
+                              ),
+                            );
+                          },
+                          onReadNotification: (id) {
+                            setState(() {
+                              final index = _notifications.indexWhere(
+                                (e) => e.message.messageId == id,
+                              );
+                              if (index != -1) {
+                                _notifications[index] =
+                                    _notifications[index].copyWith(read: true);
+                              }
+                            });
+
+                            final storage = tbContext.storage;
+                            storage.setItem(
+                              NotificationService.notificationsListKey,
+                              jsonEncode(
+                                _notifications.map((e) => e.toJson()).toList(),
+                              ),
+                            );
+
+                            NotificationService
+                                .decreaseNotificationBadgeCount();
+                          },
                         ),
                       ),
                     ],
                   ),
                 );
               }
-
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 5,
-                  vertical: 10,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2,
-                      child: SegmentedButton(
-                        segments: [
-                          ButtonSegment(
-                            value: NotificationsFilter.unread,
-                            label: Center(
-                              child: Text(
-                                'Unread',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ),
-                          ),
-                          ButtonSegment(
-                            value: NotificationsFilter.all,
-                            label: Text(
-                              'All',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ],
-                        selected: {notificationsFilter},
-                        onSelectionChanged: (newSelection) {
-                          setState(() {
-                            notificationsFilter = newSelection.first;
-                          });
-                        },
-                        showSelectedIcon: false,
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.resolveWith(
-                            (states) {
-                              if (states.contains(MaterialState.selected)) {
-                                return TbThemeUtils.tbPrimary;
-                              }
-
-                              return Colors.grey.withOpacity(0.3);
-                            },
-                          ),
-                          foregroundColor: MaterialStateProperty.resolveWith(
-                            (states) {
-                              if (states.contains(MaterialState.selected)) {
-                                return Colors.white;
-                              }
-
-                              return Colors.grey;
-                            },
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: NotificationsList(
-                        notifications: _notifications.reversed.where((e) {
-                          if (notificationsFilter ==
-                              NotificationsFilter.unread) {
-                            return !e.read;
-                          }
-
-                          return true;
-                        }).toList(),
-                        thingsboardClient: tbClient,
-                        tbContext: tbContext,
-                        onClearNotification: (id) {
-                          final notification = _notifications.firstWhere(
-                            (e) => e.message.messageId == id,
-                          );
-
-                          if (!notification.read) {
-                            NotificationService
-                                .decreaseNotificationBadgeCount();
-                          }
-
-                          setState(() {
-                            _notifications.removeWhere(
-                              (e) => e.message.messageId == id,
-                            );
-                          });
-
-                          final storage = tbContext.storage;
-                          storage.setItem(
-                            NotificationService.notificationsListKey,
-                            jsonEncode(
-                              _notifications.map((e) => e.toJson()).toList(),
-                            ),
-                          );
-                        },
-                        onReadNotification: (id) {
-                          setState(() {
-                            final index = _notifications.indexWhere(
-                              (e) => e.message.messageId == id,
-                            );
-                            if (index != -1) {
-                              _notifications[index] =
-                                  _notifications[index].copyWith(read: true);
-                            }
-                          });
-
-                          final storage = tbContext.storage;
-                          storage.setItem(
-                            NotificationService.notificationsListKey,
-                            jsonEncode(
-                              _notifications.map((e) => e.toJson()).toList(),
-                            ),
-                          );
-
-                          NotificationService.decreaseNotificationBadgeCount();
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }
-          },
+            },
+          ),
         ),
       ),
     );
@@ -224,13 +236,14 @@ class _NotificationPageState extends TbPageState<NotificationPage> {
 
   @override
   void initState() {
-    _loadNotifications();
+    _isLoadingNotifier.value = true;
+    _loadNotifications().whenComplete(
+      () => _isLoadingNotifier.value = false,
+    );
     super.initState();
   }
 
   Future<void> _loadNotifications() async {
-    _isLoadingNotifier.value = true;
-
     final storage = createAppStorage();
     final notifications = await storage.getItem(
       NotificationService.notificationsListKey,
@@ -243,7 +256,5 @@ class _NotificationPageState extends TbPageState<NotificationPage> {
           .toList()
           .cast<NotificationModel>();
     }
-
-    _isLoadingNotifier.value = false;
   }
 }
