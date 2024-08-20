92f3c5173444ee6ca9653a97f2570851cbf33a84
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-08-20 11:38:03 +0300

    Fixed the error is displayed instead of user with long name on the list of users on the "Filters" screen.

--- lib/modules/alarm/domain/entities/assignee_entity.dart
+++ lib/modules/alarm/domain/entities/assignee_entity.dart
@@ -15,11 +15,13 @@ class AssigneeEntity extends Equatable {
   factory AssigneeEntity.fromUserInfo(UserInfo info) {
     final name = '${info.firstName ?? ''} ${info.lastName ?? ''}';
     final displayName = name.length > 1 ? name : info.email;
+    final shortName =
+        '${info.firstName?[0] ?? ''}${info.lastName?[0] ?? ''}'.toUpperCase();
 
     return AssigneeEntity(
       userInfo: info,
       displayName: displayName,
-      shortName: displayName.split(' ').map((e) => e[0]).join('').toUpperCase(),
+      shortName: shortName,
     );
   }
 
