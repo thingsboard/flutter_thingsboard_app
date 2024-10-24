5e9eb5d10fdb09641a8acd2c9493a33b8c3a8aea
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-10-24 15:47:16 +0300

    Fixed an issue: The user can't close the "Alarms" screen, which was opened from the notification with "/alarms" route.

--- lib/modules/alarm/alarm_routes.dart
+++ lib/modules/alarm/alarm_routes.dart
@@ -2,8 +2,8 @@ import 'package:fluro/fluro.dart';
 import 'package:thingsboard_app/config/routes/router.dart';
 import 'package:thingsboard_app/core/context/tb_context.dart';
 import 'package:thingsboard_app/modules/alarm/presentation/view/alarm_details_page.dart';
-import 'package:thingsboard_app/modules/alarm/presentation/view/alarms_page.dart';
 import 'package:thingsboard_app/modules/alarm/presentation/view/alarms_search_page.dart';
+import 'package:thingsboard_app/modules/main/main_page.dart';
 
 class AlarmRoutes extends TbRoutes {
   AlarmRoutes(TbContext tbContext) : super(tbContext);
@@ -12,10 +12,7 @@ class AlarmRoutes extends TbRoutes {
     handlerFunc: (context, params) {
       final searchMode = params['search']?.first == 'true';
       if (!searchMode) {
-        return AlarmsPage(
-          tbContext,
-          searchMode: params['search']?.first == 'true',
-        );
+        return MainPage(tbContext, path: '/alarms');
       } else {
         return AlarmsSearchPage(
           tbContext: tbContext,
