thingsboard/flutter_thingsboard_pe_app

Dynamically configured bottom bar.

lib/main.dart
@@ -13,14 +13,13 @@ import 'package:thingsboard_app/constants/database_keys.dart';
 import 'package:thingsboard_app/firebase_options.dart';
 import 'package:thingsboard_app/locator.dart';
 import 'package:thingsboard_app/utils/services/firebase/i_firebase_service.dart';
+import 'package:thingsboard_app/utils/services/layouts/i_layout_service.dart';
 import 'package:thingsboard_app/utils/services/local_database/i_local_database_service.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:universal_platform/universal_platform.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-//  await FlutterDownloader.initialize();
-//  await Permission.storage.request();
   await Hive.initFlutter();
 
   await setUpRootDependencies();
@@ -51,39 +50,57 @@ void main() async {
   runApp(const ThingsboardApp());
 }
 
-class ThingsboardApp extends StatelessWidget {
+class ThingsboardApp extends StatefulWidget {
   const ThingsboardApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<StatefulWidget> createState() => _ThingsboardAppState();
+}
+
+class _ThingsboardAppState extends State<ThingsboardApp> {
+  @override
+  void initState() {
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
         statusBarColor: Colors.white,
         systemNavigationBarIconBrightness: Brightness.light,
       ),
     );
+    super.initState();
+  }
 
-    return WlThemeWidget(
-      getIt<ThingsboardAppRouter>().tbContext,
-      wlThemedWidgetBuilder: (context, data, wlParams) => MaterialApp(
-        scaffoldMessengerKey:
-            getIt<ThingsboardAppRouter>().tbContext.messengerKey,
-        localizationsDelegates: const [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: S.supportedLocales,
-        title: wlParams.appTitle!,
-        themeMode: ThemeMode.light,
-        theme: data,
-        darkTheme: tbDarkTheme,
-        onGenerateRoute: getIt<ThingsboardAppRouter>().router.generator,
-        navigatorObservers: [
-          getIt<ThingsboardAppRouter>().tbContext.routeObserver,
-        ],
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        getIt<ILayoutService>().setDeviceScreenSize(
+          MediaQuery.of(context).size,
+          orientation: MediaQuery.of(context).orientation,
+        );
+
+        return WlThemeWidget(
+          getIt<ThingsboardAppRouter>().tbContext,
+          wlThemedWidgetBuilder: (context, data, wlParams) => MaterialApp(
+            scaffoldMessengerKey:
+                getIt<ThingsboardAppRouter>().tbContext.messengerKey,
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.supportedLocales,
+            title: wlParams.appTitle!,
+            themeMode: ThemeMode.light,
+            theme: data,
+            darkTheme: tbDarkTheme,
+            onGenerateRoute: getIt<ThingsboardAppRouter>().router.generator,
+            navigatorObservers: [
+              getIt<ThingsboardAppRouter>().tbContext.routeObserver,
+            ],
+          ),
+        );
+      },
     );
   }
 }

thingsboard/flutter_thingsboard_app

lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/messages.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -14,6 +13,7 @@ import 'package:thingsboard_app/core/auth/login/region.dart';
 import 'package:thingsboard_app/firebase_options.dart';
 import 'package:thingsboard_app/locator.dart';
 import 'package:thingsboard_app/utils/services/firebase/i_firebase_service.dart';
+import 'package:thingsboard_app/utils/services/layouts/i_layout_service.dart';
 import 'package:thingsboard_app/utils/services/local_database/i_local_database_service.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:universal_platform/universal_platform.dart';
@@ -59,31 +59,33 @@ class ThingsboardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.white,
-        systemNavigationBarIconBrightness: Brightness.light,
-      ),
-    );
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        getIt<ILayoutService>().setDeviceScreenSize(
+          MediaQuery.of(context).size,
+          orientation: MediaQuery.of(context).orientation,
+        );
 
-    return MaterialApp(
-      scaffoldMessengerKey:
-          getIt<ThingsboardAppRouter>().tbContext.messengerKey,
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.supportedLocales,
-      onGenerateTitle: (BuildContext context) => S.of(context).appTitle,
-      themeMode: ThemeMode.light,
-      theme: tbTheme,
-      darkTheme: tbDarkTheme,
-      onGenerateRoute: getIt<ThingsboardAppRouter>().router.generator,
-      navigatorObservers: [
-        getIt<ThingsboardAppRouter>().tbContext.routeObserver,
-      ],
+        return MaterialApp(
+          scaffoldMessengerKey:
+              getIt<ThingsboardAppRouter>().tbContext.messengerKey,
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.supportedLocales,
+          onGenerateTitle: (BuildContext context) => S.of(context).appTitle,
+          themeMode: ThemeMode.light,
+          theme: tbTheme,
+          darkTheme: tbDarkTheme,
+          onGenerateRoute: getIt<ThingsboardAppRouter>().router.generator,
+          navigatorObservers: [
+            getIt<ThingsboardAppRouter>().tbContext.routeObserver,
+          ],
+        );
+      },
     );
   }
 }