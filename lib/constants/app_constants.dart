d145ccdce1e24e1a9e4887e47fd4c7e994ebcca5
Akash037 <akashrajput037@gmail.com>
2025-03-15 10:42:38 +0530

    Added thingsBoardApiEndpoint, app icon and name

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = 'http://localhost:8080';
+  static const thingsBoardApiEndpoint = 'http://http://43.204.100.33:8080';
   static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   static const thingsboardIOSAppSecret = 'Your app secret here';
