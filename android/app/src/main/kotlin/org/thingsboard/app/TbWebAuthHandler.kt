8fc94ccac6b238bf1d1643c7eac438d4353a1279
hharkovenko <hharkovenko@thingsboard.io>
2025-07-18 11:02:34 +0300

    migration to flutter 3.29.0

--- android/app/src/main/kotlin/org/thingsboard/app/TbWebAuthHandler.kt
+++ android/app/src/main/kotlin/org/thingsboard/app/TbWebAuthHandler.kt
@@ -10,7 +10,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class TbWebAuthHandler(private val context: Context): MethodCallHandler {
     companion object {
