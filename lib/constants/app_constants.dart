6fee632bca5726a75c0e5a7975fb41124e71a828
hharkovenko <hharkovenko@thingsboard.io>
2025-07-25 12:00:06 +0300

    updated app to new config

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract final class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = String.fromEnvironment('thingsBoardApiEndpoint');
+  static const thingsBoardApiEndpoint = String.fromEnvironment('thingsboardApiEndpoint');
   static const thingsboardOAuth2CallbackUrlScheme = String.fromEnvironment('thingsboardOAuth2CallbackUrlScheme');
   static const thingsboardIOSAppSecret = String.fromEnvironment('thingsboardIOSAppSecret');
   static const thingsboardAndroidAppSecret = String.fromEnvironment('thingsboardAndroidAppSecret');
