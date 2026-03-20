5c6556c72450c74bc0408c544af62cead4be77f3
Valeriia Koriavikova <mandragora1783@gmail.com>
2026-03-20 15:37:05 +0200

    Fix for OAuth icon

--- lib/core/auth/login/widgets/o_auth_buttons.dart
+++ lib/core/auth/login/widgets/o_auth_buttons.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:thingsboard_app/constants/assets_path.dart';
 import 'package:thingsboard_app/core/auth/login/widgets/o_auth_button.dart';
 import 'package:thingsboard_app/generated/l10n.dart';
+import 'package:thingsboard_app/modules/main/providers/navigation_helper.dart';
 import 'package:thingsboard_app/thingsboard_client.dart';
 
 class OAuthButtons extends StatelessWidget {
@@ -17,38 +18,48 @@ class OAuthButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return clients.length == 1 && clients.first.name == 'qr'
         ? Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            OAuthButton(
-              logo: SvgPicture.asset(
-                ThingsboardImage.oauth2Logos['qr-code-logo']!,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OAuthButton(
+                logo: SvgPicture.asset(
+                  ThingsboardImage.oauth2Logos['qr-code-logo']!,
+                ),
+                onTap: () => onButtonPressed(clients.first),
+                title: S.of(context).scanQrCode,
               ),
-              onTap: () => onButtonPressed(clients.first),
-              title: S.of(context).scanQrCode,
-            ),
-          ],
-        )
+            ],
+          )
         : Row(
-          spacing: 10,
-          children:
-              clients
-                  .map(
-                    (e) => Expanded(
-                      child: OAuthButton(
-                        logo: getOauth2ButtonLogo(e),
-                        onTap: () => onButtonPressed(e),
-                      ),
+            spacing: 10,
+            children: clients
+                .map(
+                  (e) => Expanded(
+                    child: OAuthButton(
+                      logo: _getOauth2ButtonLogo(context, e),
+                      onTap: () => onButtonPressed(e),
                     ),
-                  )
-                  .toList(),
-        );
+                  ),
+                )
+                .toList(),
+          );
   }
 }
 
-Widget getOauth2ButtonLogo(OAuth2ClientInfo info) {
+Widget _getOauth2ButtonLogo(BuildContext context, OAuth2ClientInfo info) {
+  final String? svgPath = ThingsboardImage.oauth2Logos[info.icon];
+
+  if (svgPath != null) {
+    return SizedBox(width: 24, height: 24, child: SvgPicture.asset(svgPath));
+  }
+
+  final iconData = NavigationHelper.getIconFromString(info.icon);
+
   return SizedBox(
     width: 24,
     height: 24,
-    child: SvgPicture.asset(ThingsboardImage.oauth2Logos[info.icon]!),
+    child: Icon(
+      iconData,
+      color: Colors.black,
+    ),
   );
 }
